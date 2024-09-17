fbd4a01baaccba73af419beb5aa066767fcb22cc
Matheus Dantas <129445416+theussilvas@users.noreply.github.com>
2024-09-17 21:20:42 +0000

    customiza

--- alugueltemas/src/app/components/temasperiodo/temasperiodo.component.ts
+++ alugueltemas/src/app/components/temasperiodo/temasperiodo.component.ts
@@ -4,11 +4,12 @@ import { TemasService } from '../../services/Temas/temas.service';
 import { AluguelService } from '../../services/Aluguel/aluguel.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-temasperiodo',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, CalendarModule],
   templateUrl: './temasperiodo.component.html',
   styleUrl: './temasperiodo.component.css'
 })
@@ -19,14 +20,11 @@ export class TemasperiodoComponent implements OnInit{
   mesSelecionado:number = 1;
   anoSelecionado:number = new Date().getFullYear();
   resultado: { [key: string]: number } = {};
+  dataSelecionada:string = '';
+  date2: Date | undefined;
 
   constructor(private temasServices:TemasService, private alugueisServices: AluguelService){}
   
-
-  meses: string[] = [
-    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
-    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
-  ];
 
   ngOnInit(){
       this.iniciar()
@@ -49,6 +47,12 @@ export class TemasperiodoComponent implements OnInit{
   }
 
   temasPeriodo(){
+    if (this.dataSelecionada){
+      const data = new Date(this.dataSelecionada);
+      this.mesSelecionado = data.getMonth() + 1; 
+      this.anoSelecionado = data.getFullYear(); 
+      
+    }
     this.resultado = this.temasServices.temasPeriodo(this.temas,this.alugueis,this.mesSelecionado,this.anoSelecionado)
   }
 
